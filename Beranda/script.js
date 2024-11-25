3659125a12ca6bb0b95ad6d31e67f6ffa76f40c0
arswendoerz <arswendoerza00@gmail.com>
2024-11-25 23:05:24 +0700

    first commit

---
+++ Beranda/script.js
@@ -0,0 +1,10 @@
+// Initialization for ES Users
+let time = document.getElementById("time");
+    setInterval(() =>{
+      let d = new Date();
+      time.innerHTML = d.toLocaleTimeString();
+    },1000)
+
+    document.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+    });
---
+++ SejarahGereja/script.js
@@ -0,0 +1,9 @@
+let time = document.getElementById("time");
+    setInterval(() =>{
+      let d = new Date();
+      time.innerHTML = d.toLocaleTimeString();
+    },1000)
+
+    document.addEventListener("contextmenu", (e) => {
+        e.preventDefault();
+    });
